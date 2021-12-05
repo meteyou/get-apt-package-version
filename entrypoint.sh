015391b8e8e14eefb2dc7175776109cee87c5d0a
Dang Mai <contact@dangmai.net>
2021-12-04 20:07:00 -0500

    Use absolute path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-cd get-apt-package-version
-docker build --build-arg BASE_IMAGE=$1 -t apt-package-version .
+docker build --build-arg BASE_IMAGE=$1 -t apt-package-version /get-apt-package-version
 shift
 docker run --rm apt-package-version "$@"
