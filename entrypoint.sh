17919610f6c415dff6e652df04fecd57ee5b4c19
Stefan Dej <meteyou@gmail.com>
2024-07-12 18:32:09 +0200

    fix: fix github output in get-package-version.sh
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 docker build --build-arg BASE_IMAGE=$1 -t apt-package-version /get-apt-package-version
 shift
-docker run --rm apt-package-version "$@" sh -c 'echo "version=$VERSION"' >> $GITHUB_OUTPUT
+#docker run --rm apt-package-version "$@"
+docker run -d --name apt-package-version apt-package-version "$@"
+docker exec sh -c 'echo "version=$VERSION"' >> $GITHUB_OUTPUT
+docker stop apt-package-version
+docker rm apt-package-version
